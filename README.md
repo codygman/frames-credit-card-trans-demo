ef2e161ba5be091c6ecf4d82286c5f22a0bfc01f
Cody Goodman <codygman.consulting@gmail.com>
2016-12-31 01:46:36 -0600

    WIP

---
+++ README.md
@@ -0,0 +1,29 @@
+Print some data
+```haskell
+λ> pipePreview transactions 5 cat
+{Service Area :-> "Childrens Services", Account Description :-> "IT Services", Creditor :-> "123-REG.CO.UK", Transaction Date :-> Chicago (TimeIn 2014-04-23 00:00:00 CDT), JV Reference :-> 93, JV Date :-> Chicago (TimeIn 2014-05-20 00:00:00 CDT), JV Value :-> 143.81}
+{Service Area :-> "Childrens Services", Account Description :-> "Equipment and Materials Repair", Creditor :-> "AFE SERVICELINE", Transaction Date :-> Chicago (TimeIn 2014-04-02 00:00:00 CDT), JV Reference :-> 6, JV Date :-> Chicago (TimeIn 2014-05-20 00:00:00 CDT), JV Value :-> 309.38}
+{Service Area :-> "Childrens Services", Account Description :-> "Equipment and Materials Repair", Creditor :-> "AFE SERVICELINE", Transaction Date :-> Chicago (TimeIn 2014-04-02 00:00:00 CDT), JV Reference :-> 7, JV Date :-> Chicago (TimeIn 2014-05-20 00:00:00 CDT), JV Value :-> 218.76}
+{Service Area :-> "Childrens Services", Account Description :-> "Building Repairs & Maintenance", Creditor :-> "ALLSOP & FRANCIS", Transaction Date :-> Chicago (TimeIn 2014-04-15 00:00:00 CDT), JV Reference :-> 381, JV Date :-> Chicago (TimeIn 2014-05-20 00:00:00 CDT), JV Value :-> 306.0}
+{Service Area :-> "Childrens Services", Account Description :-> "Equipment and Materials Purcha", Creditor :-> "APPLE ITUNES STORE-", Transaction Date :-> Chicago (TimeIn 2014-04-18 00:00:00 CDT), JV Reference :-> 207, JV Date :-> Chicago (TimeIn 2014-05-20 00:00:00 CDT), JV Value :-> 7.32}
+```
+
+Let's get all data from 2014-04-02:
+
+```haskell
+```
+
+
+```haskell
+```
+
+```haskell
+```
+```haskell
+```
+```haskell
+```
+```haskell
+```
+```haskell
+```
--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,57 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
 import Frames
+import Frames.CSV
+import Pipes hiding (Proxy)
+import qualified Pipes.Prelude as P
+import Frames.Time.Chicago.Columns
+import Frames.Time.Chicago.TimeIn
+import Data.Time
+
+
+tableTypes' rowGen { rowTypeName = "Transaction"
+                   , columnUniverse = $(colQ ''MyColumns)
+                   }
+  "data/purchasing-card-data-2014.csv"
+
+transactions :: Producer Transaction IO ()
+transactions = readTable  "data/purchasing-card-data-2014.csv"
+
+-- helpers
+chicagoToZoned = (\(Chicago (TimeIn zt)) -> zt)
+d = fromGregorian
+
+-- between meaning on or after start but before end
+transactionDateBetween :: (TransactionDate ∈ rs) =>
+               Day
+            -> Day
+            -> Pipe (Record rs) (Record rs) IO r
+transactionDateBetween start end = P.filter go
+  where go r = let targetDate = rget transactionDate r -- :: Chicago
+                   targetDate' = chicagoToZoned targetDate -- :: ZonedTime
+                   targetDay = localDay (zonedTimeToLocalTime targetDate') :: Day
+               in
+                 targetDay >= start && targetDay < end
+
+
+-- then we can answer something like: How many transactions happened in the month of April
+-- λ> P.length (transactions >-> transactionDateBetween (d 2014 4 1) (d 2014 4 30))
+-- 314
+
+-- using pipePreview
+-- λ> pipePreview transactions 3 (transactionDateBetween (d 2014 4 1) (d 2014 4 30))
+-- {Service Area :-> "Childrens Services", Account Description :-> "IT Services", Creditor :-> "123-REG.CO.UK", Transaction Date :-> Chicago (TimeIn 2014-04-23 00:00:00 CDT), JV Reference :-> 93, JV Date :-> Chicago (TimeIn 2014-05-20 00:00:00 CDT), JV Value :-> 143.81}
+-- {Service Area :-> "Childrens Services", Account Description :-> "Equipment and Materials Repair", Creditor :-> "AFE SERVICELINE", Transaction Date :-> Chicago (TimeIn 2014-04-02 00:00:00 CDT), JV Reference :-> 6, JV Date :-> Chicago (TimeIn 2014-05-20 00:00:00 CDT), JV Value :-> 309.38}
+-- {Service Area :-> "Childrens Services", Account Description :-> "Equipment and Materials Repair", Creditor :-> "AFE SERVICELINE", Transaction Date :-> Chicago (TimeIn 2014-04-02 00:00:00 CDT), JV Reference :-> 7, JV Date :-> Chicago (TimeIn 2014-05-20 00:00:00 CDT), JV Value :-> 218.76}
 
 
 
 main :: IO ()
 main = do
+  -- runEffect $ transactions >-> transactionDateBetween (fromGregorian 2014 4 1) (fromGregorian 2014 4 2) >-> P.take 5 >-> P.print
+  runEffect $ transactions >-> transactionDateBetween (fromGregorian 2014 4 2) (fromGregorian 2014 4 3)  >-> P.take 5 >-> P.print
   putStrLn "hello world"

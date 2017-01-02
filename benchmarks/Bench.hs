4c4a63ba0bf5447180004603ec3593259f4ed871
Cody Goodman <codygman.consulting@gmail.com>
2017-01-02 15:03:29 -0600

    update benchmarks

--- benchmarks/Bench.hs
+++ benchmarks/Bench.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RankNTypes #-}
@@ -8,6 +11,7 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 import Criterion.Main
 
+
 import Frames
 import Frames.CSV
 import Frames.Diff
@@ -16,6 +20,11 @@ import qualified Frames.Time.Chicago.Columns as C
 import qualified Pipes as P
 import qualified Pipes.Prelude as P
 import Pipes hiding (Proxy)
+import Control.Concurrent (threadDelay)
+
+import Control.DeepSeq.Generics (genericRnf)
+import Control.DeepSeq
+import GHC.Generics
 
 tableTypes' rowGen { rowTypeName = "TransactionTime"
                    , columnUniverse = $(colQ ''C.MyColumns)
@@ -33,13 +42,40 @@ transactionsTime = readTable  "data/purchasing-card-data-2014-large.csv"
 transactionsThyme :: Producer TransactionThyme IO ()
 transactionsThyme = readTable  "data/purchasing-card-data-2014-large.csv"
 
-
 -- Our benchmark harness.
-main = defaultMain [ bgroup "length" [ bench "time 5K"  (nfIO (P.length (transactionsTime P.>-> P.take 5000)))
-                                     -- , bench "thyme 5K"  $ P.length (transactionsThyme P.>-> P.take 5000)
-                                     -- , bench "time 50K"  $ nf P.length (transactionsTime P.>-> P.take 50000)
-                                     -- , bench "thyme 50K"  $ nf P.length (transactionsThyme P.>-> P.take 50000)
-                                     ]
-                   , bgroup "datebetween" [ bench "datebetween on 5000 time" $ nfIO $ P.length (transactionsTime P.>-> P.take 5000 P.>-> dateBetween transactionDate(dt 2014 4 1) (dt 2014 4 30))
-                                          ]
-  ]
+main = defaultMain [ bgroup "with time"
+                       [ bench "length 5K"  (nfIO (P.length (transactionsTime P.>-> P.take 5000)))
+                       , bench "length 100K"  (nfIO (P.length (transactionsTime P.>-> P.take 100000)))
+
+                       , bench "datebetween 5K" $ nfIO $ P.length (
+                           transactionsTime P.>-> P.take 5000 P.>-> dateBetween transactionDate(dt 2014 4 1) (dt 2014 4 30)
+                           )
+
+                       -- , bench "datebetween on 100000" $ nfIO $ P.length (
+                       --     transactionsTime P.>-> P.take 100000 P.>-> dateBetween transactionDate(dt 2014 4 1) (dt 2014 4 30)
+                       --     )
+
+                       -- TODO requires NFData instance for Frames/FrameRec
+                       -- , bench "innerjoin on 5000" $ nfIO $
+                       --     innerJoin
+                       --      (transactionsTime P.>-> P.take 5000)
+                       --      creditor
+                       --      (transactionsTime P.>-> P.take 5000)
+                       --      creditor
+                       -- ]
+
+                   , bgroup "with thyme"
+                       [ bench "length 5K"  (nfIO (P.length (transactionsThyme P.>-> P.take 5000)))
+                       , bench "length 100K"  (nfIO (P.length (transactionsThyme P.>-> P.take 100000)))
+
+                       , bench "datebetween 5k" $ nfIO $ P.length (
+                           transactionsThyme P.>-> P.take 5000 P.>-> dateBetween transactionDate(dt 2014 4 1) (dt 2014 4 30)
+                           )
+
+                       -- , bench "datebetween on 100000" $ nfIO $ P.length (
+                       --     transactionsThyme P.>-> P.take 100000 P.>-> dateBetween transactionDate(dt 2014 4 1) (dt 2014 4 30)
+                       --     )
+
+
+                       ]
+                   ]
